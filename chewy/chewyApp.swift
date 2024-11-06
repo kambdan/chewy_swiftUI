12101cb3e3fd17c56c1d26ef541fbb96b9682965
kambdan <juancambi1319@gmail.com>
2024-11-05 21:32:43 -0500

    Initial Commit

---
+++ chewy/chewyApp.swift
@@ -0,0 +1,17 @@
+//
+//  chewyApp.swift
+//  chewy
+//
+//  Created by Juan Cambizaca on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct chewyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chewyTests/chewyTests.swift
@@ -0,0 +1,17 @@
+//
+//  chewyTests.swift
+//  chewyTests
+//
+//  Created by Juan Cambizaca on 5/11/24.
+//
+
+import Testing
+@testable import chewy
+
+struct chewyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
